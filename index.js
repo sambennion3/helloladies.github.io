dfed559fd92b2c2651f36081588fcc04d7a0a59b
sambennion3 <70674273+sambennion3@users.noreply.github.com>
2022-11-22 17:01:01 -0700

    Committing the first changes

---
+++ index.js
@@ -0,0 +1,13 @@
+const router = async () => {
+
+
+    const routes = [
+        {path: "/", view: () => console.log("Viewing homepage")}
+    ];
+    const switcher = document.querySelector('.button');
+    switcher.addEventListener('click', function(){
+//add the stuff here to send it to the google doc
+    });
+
+    
+}
